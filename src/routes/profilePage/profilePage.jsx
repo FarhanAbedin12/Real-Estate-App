f69cf70bf8d0ce3fe12b186a9786bf3a189b04a4
Eusra <urbiibnat07@gmail.com>
2024-12-21 20:11:37 +0600

    UI completed

--- src/routes/profilePage/profilePage.jsx
+++ src/routes/profilePage/profilePage.jsx
@@ -1,9 +1,49 @@
-import "./profilePage.scss"
+import Chat from "../../components/chat/Chat";
+import List from "../../components/list/List";
+import "./profilePage.scss";
 
-function ProfilePage(){
-    return (
-        <div className="profilePage">ProfilePage</div>
-    )
+function ProfilePage() {
+  return (
+    <div className="profilePage">
+      <div className="details">
+        <div className="wrapper">
+          <div className="title">
+            <h1>User Information</h1>
+            <button>Update Profile</button>
+          </div>
+          <div className="info">
+            <span>
+              Avatar:
+              <img
+                src="https://www.hindustantimes.com/ht-img/img/2023/11/23/1600x900/IMG_7289_1700764001828_1700764009852.jpeg"
+                alt=""
+              />
+            </span>
+            <span>
+              Username: <b>Nanami</b>
+            </span>
+            <span>
+              E-mail: <b>john@gmail.com</b>
+            </span>
+          </div>
+          <div className="title">
+            <h1>My List</h1>
+            <button>Create New Post</button>
+          </div>
+          <List />
+          <div className="title">
+            <h1>Saved List</h1>
+          </div>
+          <List />
+        </div>
+      </div>
+      <div className="chatContainer">
+        <div className="wrapper">
+          <Chat/>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ProfilePage
+export default ProfilePage;
