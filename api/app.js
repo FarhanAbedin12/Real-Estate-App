0526170cb92ae14d5b259a24fbbde4ab5405d671
Eusra <urbiibnat07@gmail.com>
2025-01-07 18:08:46 +0600

    Auth requirements

---
+++ api/app.js
@@ -0,0 +1,24 @@
+import cookieParser from "cookie-parser";
+import express from "express";
+
+import authRoute from "./routes/auth.route.js";
+// import propRoute from "./routes/prop.route.js";
+// import userRoute from "./routes/user.route.js";
+
+const app = express();
+app.use(express.json());
+
+app.use(cookieParser());
+
+// app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+// app.use(cookieParser());
+
+app.use("/api/auth", authRoute);
+// app.use("/api/prop", propRoute);
+// app.use("/api/user", userRoute);
+
+export default app;
+
+app.listen(8800, () => {
+  console.log("Server is on port 8800!");
+});
